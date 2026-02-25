7d81ce2c51f53d789dfd80f5ad5a69c28684f2b0
CosmicCoder06 <k.avhi4612@gmail.com>
2026-02-25 22:40:38 +0530

    index.jsjfixed

--- Class_Work/Learning_Modules/index.js
+++ Class_Work/Learning_Modules/index.js
@@ -1,5 +1,5 @@
 const calculate = require("./Calculation");
-// const sub = require("./Calculation");
+
 console.log(calculate.sum(4, 5));
 console.log(calculate.sub(4, 5));
 //now this will reffer to jason file
